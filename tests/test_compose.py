b9230e361c601f8eb72e6ea1184de2a452e202c1
Dag Brattli <dag.brattli@cognite.com>
2020-10-17 12:04:47 +0200

    Test compose associative

--- tests/test_compose.py
+++ tests/test_compose.py
@@ -45,20 +45,6 @@ def test_compose_3(x):
 
 
 @given(st.integers())
-def test_compose_associative(x):
-    """Rearranging the parentheses in an expression will not change the result."""
-
-    fn = lambda x: x + 42
-    gn = lambda x: x - 3
-    hn = lambda x: x * 2
-
-    an = compose(fn, compose(gn, hn))
-    bn = compose(compose(fn, gn), hn)
-
-    assert an(x) == bn(x)
-
-
-@given(st.integers())
 def test_compose_many(x):
     fn = lambda x: x + 42
     gn = lambda x: x - 3
@@ -67,3 +53,39 @@ def test_compose_many(x):
     cn = compose(fn, gn, hn, fn, hn, gn, fn)
 
     assert cn(x) == fn(gn(hn(fn(hn(gn(fn(x)))))))
+
+
+@given(st.integers())
+def test_compose_rigth_identity(x):
+    fn = lambda x: x + 42
+
+    cn = compose(fn, identity)
+
+    assert cn(x) == fn(x)
+
+
+@given(st.integers())
+def test_compose_left_identity(x):
+    fn = lambda x: x + 42
+
+    cn = compose(identity, fn)
+
+    assert cn(x) == fn(x)
+
+
+@given(st.integers(), st.integers(), st.integers())
+def test_compose_associative(x, y, z):
+    """Rearranging the parentheses in an expression will not change the result."""
+    fn = lambda a: a + x
+    gn = lambda b: b - y
+    hn = lambda c: c * z
+
+    cn = compose(fn, gn, hn)
+    cn_ = lambda x: hn(gn(fn(x)))
+
+    rn = compose(fn, compose(gn, hn))
+    rn_ = lambda x: (lambda b: hn(gn(b)))(fn(x))  # right associative
+    ln = compose(compose(fn, gn), hn)
+    ln_ = lambda x: hn((lambda b: gn(fn(b)))(x))  # left associative
+
+    assert cn(x) == cn_(x) == rn(x) == rn_(x) == ln(x) == ln_(x)
