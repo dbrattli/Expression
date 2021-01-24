9018f551c038c2bb140f39f5429f874bc1ddd253
Miller Wilt <miller.wilt@gmail.com>
2021-01-24 02:51:23 -0500

    Implement the catch utility decorator (#16)
    * Implement the catch utility decorator
    
    Co-authored-by: Erlend vollset <erlendvollset@gmail.com>
    Co-authored-by: Dag Brattli <dag.brattli@cognite.com>

---
+++ expression/extra/result/catch.py
@@ -0,0 +1,49 @@
+from functools import wraps
+from typing import Any, Callable, Optional, Type, TypeVar, Union, cast, overload
+
+from expression.core import Error, Ok, Result
+
+TSource = TypeVar("TSource")
+TError = TypeVar("TError", bound=Exception)
+TError_ = TypeVar("TError_", bound=Exception)
+
+
+@overload
+def catch(
+    exception: Type[TError_],
+) -> Callable[
+    [Callable[..., Union[TSource, Result[TSource, TError]]]], Callable[..., Result[TSource, Union[TError, TError_]]]
+]:
+    ...
+
+
+@overload
+def catch(f: Callable[..., TSource], *, exception: Type[TError]) -> Callable[..., Result[TSource, TError]]:
+    ...
+
+
+def catch(
+    f: Optional[Callable[..., TSource]] = None, *, exception: Type[TError]
+) -> Callable[[Callable[..., TSource]], Union[Callable[..., Result[TSource, TError]], Result[TSource, TError]]]:
+    def decorator(fn: Callable[..., TSource]) -> Callable[..., Result[TSource, TError]]:
+        @wraps(fn)
+        def wrapper(*args: Any, **kwargs: Any) -> Result[TSource, TError]:
+            try:
+                out = fn(*args, **kwargs)
+            except exception as exn:
+                return Error(cast("TError", exn))
+            else:
+                if isinstance(out, Result):
+                    return cast(Result[TSource, TError], out)
+
+                return Ok(out)
+
+        return wrapper
+
+    if f is not None:
+        return decorator(f)
+
+    return decorator
+
+
+__all__ = ["catch"]
