8f2a43eac41cd4449e67c68a93d6ad94ffb1d746
mat <matt@techspace.asia>
2023-01-11 07:05:54 +0800

    Put test in place

--- tests/test_map.py
+++ tests/test_map.py
@@ -5,6 +5,7 @@ from hypothesis import strategies as st
 
 from expression import pipe
 from expression.collections import Block, Map, map
+from expression.core.option import Some
 
 
 def test_map_empty():
@@ -123,3 +124,37 @@ def test_map_iterate(xs: Dict[str, int]):
     ys = [k for k in map.of(**xs)]
 
     assert sorted(ys) == sorted(list(xs.keys()))
+
+
+# @given(
+#     st.dictionaries(keys=st.text(), values=st.integers()),
+#     st.dictionaries(keys=st.text(), values=st.integers()),
+#     st.text(),
+#     st.text(),
+# )
+# def test_map_change(d_1: Dict[str, int], d_2: Dict[str, int], key_1: str, key_2: str):
+def test_map_change():
+    d_1 = {}
+    d_2 = {}
+    key_1 = "a"
+    key_2 = "b"
+    m: Map[str, Dict[str, int]] = Map.empty()
+    m = m.add(key_1, d_1)
+    m = m.add(key_2, d_2)
+    m = m.change(key_2, lambda x: Some({**x.value, "other": 42}))
+    # m = m.change(key_1, lambda x: Some({**x.value, "some key": 5}))
+
+    def verify(
+        original: Dict[str, int], map_key: str, override_key: str, override_value: int
+    ):
+        value = m.get(map_key)
+        assert value
+        assert override_key in value
+        for k, v in value.items():
+            if k == override_key:
+                assert v == override_value
+            else:
+                assert v == original[k]
+
+    # verify(d_1, key_1, "some key", 5)
+    verify(d_2, key_2, "other", 42)
