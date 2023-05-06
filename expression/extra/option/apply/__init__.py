c7dc5629ca4957e18c8cc84d3f5051518d4b5f73
phi <phi.friday@gmail.com>
2023-05-06 23:58:19 +0900

    fix: apply.py -> apply/main.py

---
+++ expression/extra/option/apply/__init__.py
@@ -0,0 +1,3 @@
+from .main import call, func, of_iterable, of_obj
+
+__all__ = ["of_obj", "of_iterable", "func", "call"]
--- tests/test_apply.py
+++ tests/test_apply.py
@@ -4,7 +4,9 @@ from expression import Ok, Some, option, pipe, result
 from expression.core.option import BaseOption
 from expression.core.result import BaseResult
 from expression.extra.option import apply as option_apply
+from expression.extra.option.apply import main as option_main
 from expression.extra.result import apply as result_apply
+from expression.extra.result.apply import main as result_main
 
 RESULT = "result"
 DUMMY = "dummy"
@@ -14,7 +16,7 @@ def test_option_var():
     value = 1
     var = option_apply.of_obj(value)
 
-    assert isinstance(var, option_apply.Var)
+    assert isinstance(var, option_main.Var)
     assert pipe(var.value, option.is_some) is True
     assert pipe(var.value, option.is_none) is False
     assert var.value.default_value(0) == value
@@ -24,7 +26,7 @@ def test_option_seq():
     value = (1, "q", 3)
     seq = option_apply.of_iterable(*value)
 
-    assert isinstance(seq, option_apply.Seq)
+    assert isinstance(seq, option_main.Seq)
     assert pipe(seq.value, option.is_some) is True
     assert pipe(seq.value, option.is_none) is False
     assert seq.value.default_value((-1, "w", 10)) == value
@@ -33,7 +35,7 @@ def test_option_seq():
 def test_option_func():
     func = option_apply.func(_func_two)
 
-    assert isinstance(func, option_apply.Func)
+    assert isinstance(func, option_main.Func)
     assert pipe(func.value, option.is_some) is True
     assert pipe(func.value, option.is_none) is False
     assert func.value.default_value(_other_func_two) is _func_two
@@ -57,7 +59,7 @@ def test_option_var_to_seq():
     seq_0, seq_1 = var_0 * var_1, var_1 * var_0
 
     for seq in seq_0, seq_1:
-        assert isinstance(seq, option_apply.Seq)
+        assert isinstance(seq, option_main.Seq)
     assert seq_0.value.default_value(dummy_0) == (value_0, value_1)
     assert seq_1.value.default_value(dummy_1) == (value_1, value_0)
 
@@ -69,7 +71,7 @@ def test_option_var_func():
     assert dummy != output
 
     var = option_apply.of_obj(value)
-    assert isinstance(var, option_apply.Var)
+    assert isinstance(var, option_main.Var)
     func_0 = _func_one * var
     func_1 = option_apply.func(_func_one) * var
     var_0, var_1 = func_0 * option_apply.call, func_1 * option_apply.call
@@ -86,7 +88,7 @@ def test_option_seq_func():
     assert dummy != output
 
     seq = option_apply.of_iterable(*values)
-    assert isinstance(seq, option_apply.Seq)
+    assert isinstance(seq, option_main.Seq)
 
     func = option_apply.func(_func_two) * seq
     var = func * option_apply.call
@@ -117,7 +119,7 @@ def test_result_var():
     value = 1
     var = result_apply.of_obj(value)
 
-    assert isinstance(var, result_apply.Var)
+    assert isinstance(var, result_main.Var)
     assert pipe(var.value, result.is_ok) is True
     assert pipe(var.value, result.is_error) is False
     assert var.value.default_value(0) == value
@@ -127,7 +129,7 @@ def test_result_seq():
     value = (1, "q", 3)
     seq = result_apply.of_iterable(*value)
 
-    assert isinstance(seq, result_apply.Seq)
+    assert isinstance(seq, result_main.Seq)
     assert pipe(seq.value, result.is_ok) is True
     assert pipe(seq.value, result.is_error) is False
     assert seq.value.default_value((-1, "w", 10)) == value
@@ -136,7 +138,7 @@ def test_result_seq():
 def test_result_func():
     func = result_apply.func(_func_two)
 
-    assert isinstance(func, result_apply.Func)
+    assert isinstance(func, result_main.Func)
     assert pipe(func.value, result.is_ok) is True
     assert pipe(func.value, result.is_error) is False
     assert func.value.default_value(_other_func_two) is _func_two
@@ -160,7 +162,7 @@ def test_result_var_to_seq():
     seq_0, seq_1 = var_0 * var_1, var_1 * var_0
 
     for seq in seq_0, seq_1:
-        assert isinstance(seq, result_apply.Seq)
+        assert isinstance(seq, result_main.Seq)
     assert seq_0.value.default_value(dummy_0) == (value_0, value_1)
     assert seq_1.value.default_value(dummy_1) == (value_1, value_0)
 
@@ -172,7 +174,7 @@ def test_result_var_func():
     assert dummy != output
 
     var = result_apply.of_obj(value)
-    assert isinstance(var, result_apply.Var)
+    assert isinstance(var, result_main.Var)
     func_0 = _func_one * var
     func_1 = result_apply.func(_func_one) * var
     var_0, var_1 = func_0 * result_apply.call, func_1 * result_apply.call
@@ -189,7 +191,7 @@ def test_result_seq_func():
     assert dummy != output
 
     seq = result_apply.of_iterable(*values)
-    assert isinstance(seq, result_apply.Seq)
+    assert isinstance(seq, result_main.Seq)
 
     func = result_apply.func(_func_two) * seq
     var = func * result_apply.call
@@ -225,7 +227,7 @@ def test_option_partial_func():
         option_apply.of_obj(value_1),
         option_apply.of_obj(value_2),
     )
-    partial_one_func = option_apply.Func(Some(partial(_func_three, value_0)))
+    partial_one_func = option_main.Func(Some(partial(_func_three, value_0)))
 
     result_0 = func * var_0 * var_1 * var_2 * option_apply.call
     result_1 = partial_one_func * var_1 * var_2 * option_apply.call
@@ -233,7 +235,7 @@ def test_option_partial_func():
 
     seq = option_apply.of_iterable(value_0, value_1)
     assert seq == var_0 * var_1
-    partial_two_func = option_apply.Func(Some(partial(_func_three, value_0, value_1)))
+    partial_two_func = option_main.Func(Some(partial(_func_three, value_0, value_1)))
 
     result_2 = func * seq * var_2 * option_apply.call
     result_3 = partial_two_func * var_2 * option_apply.call
@@ -249,7 +251,7 @@ def test_result_partial_func():
         result_apply.of_obj(value_1),
         result_apply.of_obj(value_2),
     )
-    partial_one_func = result_apply.Func(Ok(partial(_func_three, value_0)))
+    partial_one_func = result_main.Func(Ok(partial(_func_three, value_0)))
 
     result_0 = func * var_0 * var_1 * var_2 * result_apply.call
     result_1 = partial_one_func * var_1 * var_2 * result_apply.call
@@ -257,7 +259,7 @@ def test_result_partial_func():
 
     seq = result_apply.of_iterable(value_0, value_1)
     assert seq == var_0 * var_1
-    partial_two_func = result_apply.Func(Ok(partial(_func_three, value_0, value_1)))
+    partial_two_func = result_main.Func(Ok(partial(_func_three, value_0, value_1)))
 
     result_2 = func * seq * var_2 * result_apply.call
     result_3 = partial_two_func * var_2 * result_apply.call
