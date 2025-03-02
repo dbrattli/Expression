774f1421d25a7d8bd79e59a269daee4252819528
Dag Brattli <dag.brattli@cognite.com>
2025-03-02 09:28:40 +0100

    Async results

--- expression/effect/__init__.py
+++ expression/effect/__init__.py
@@ -4,9 +4,11 @@ from .option import OptionBuilder as option
 from .result import ResultBuilder as result
 from .result import TryBuilder as try_
 from .seq import SeqBuilder as seq_builder
+from .async_result import AsyncResultBuilder as async_result
+from .async_result import AsyncTryBuilder as async_try
 
 
 seq = seq_builder
 
 
-__all__ = ["option", "result", "seq", "try_"]
+__all__ = ["option", "result", "seq", "try_", "async_result", "async_try"]
