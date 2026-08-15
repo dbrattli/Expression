c7e1e80ea9ae7a559509fb9fac70dfcbb7053af1
Your Name <you@example.com>
2026-08-15 09:53:25 +0200

    chore: migrate project tooling to uv

--- scripts/shipit_bump_version.sh
+++ scripts/shipit_bump_version.sh
@@ -13,7 +13,6 @@ pep440=$(printf '%s\n' "$semver" | sed -E \
   -e 's/-beta\.([0-9]+)$/b\1/' \
   -e 's/-rc\.([0-9]+)$/rc\1/')
 
-poetry version "$pep440" >/dev/null
 printf '__version__ = "%s"\n' "$pep440" > expression/_version.py
 
 echo "Bumped version to $pep440 (from SemVer $semver)"
